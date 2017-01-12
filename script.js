a062928ee950d3111211b7003920e64f03270c30
George Siaskov <egor.record@yandex.ru>
2017-01-12 15:03:20 +0300

    Сделал изменение цены и изменение счета

--- script.js
+++ script.js
@@ -3,23 +3,12 @@
  */
 
 var productsApp = angular.module('productsApp', []);
+
 productsApp.controller('ProductCtrl', function ($scope, $http){
     $http.get('products.json').success(function(data) {
         $scope.products = data;
 
-        $scope.products.quantity = 1;
-
-
-        console.log($scope.products);
-
     });
-
-
-    $scope.quantityUp = function (product) {
-        $scope.products.quantity++;
-    };
-
-    $scope.active = false;
 
     $scope.quantityDown = function (product) {
 
@@ -29,5 +18,63 @@ productsApp.controller('ProductCtrl', function ($scope, $http){
 
     };
 
+}).directive("down", function(){
 
+   return function(scope, element) {
+       element.bind("click", function(){
+           var count = this.parentNode.childNodes[0].nextSibling.value;
+
+           if (count > 1) {
+               count--;
+           }
+
+           this.parentNode.childNodes[0].nextSibling.value = count;
+           var packaging = (parseInt(count) * 2.47).toFixed(2);
+
+           this.parentNode.parentNode.parentNode.parentNode.children[10].querySelector('.unit--infoInn').innerHTML = count + " упак. = " + packaging + " м. кв.";
+
+       })
+   }
+}).directive("up", function(){
+
+    return function(scope, element) {
+        element.bind("click", function(){
+            var count = this.parentNode.childNodes[0].nextSibling.value;
+
+            count++;
+
+            this.parentNode.childNodes[0].nextSibling.value = count;
+            var packaging = (parseInt(count) * 2.47).toFixed(2);
+
+            this.parentNode.parentNode.parentNode.parentNode.children[10].querySelector('.unit--infoInn').innerHTML = count + " упак. = " + packaging + " м. кв.";
+
+
+        })
+    }
+}).directive("topackaging", function(){
+
+    return function(scope, element) {
+        element.bind("click", function(){
+
+            var unitGold = this.parentNode.parentNode.parentNode.children[6].querySelector('.goldPrice').getAttribute('data-unit-price')
+                unit = this.parentNode.parentNode.parentNode.children[7].querySelector('.retailPrice').getAttribute('data-unit-price');
+            this.parentNode.parentNode.parentNode.children[6].querySelector('.goldPrice').innerHTML = unitGold;
+            this.parentNode.parentNode.parentNode.children[7].querySelector('.retailPrice').innerHTML = unit;
+
+        })
+    }
+}).directive("tometer", function(){
+
+    return function(scope, element) {
+        element.bind("click", function(){
+
+            var unitGold = this.parentNode.parentNode.parentNode.children[6].querySelector('.goldPrice').getAttribute('data-default-price')
+            unit = this.parentNode.parentNode.parentNode.children[7].querySelector('.retailPrice').getAttribute('data-default-price');
+            this.parentNode.parentNode.parentNode.children[6].querySelector('.goldPrice').innerHTML = unitGold;
+            this.parentNode.parentNode.parentNode.children[7].querySelector('.retailPrice').innerHTML = unit;
+
+        })
+    }
 });
+
+
